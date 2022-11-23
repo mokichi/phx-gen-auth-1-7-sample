0fc382a352360fac770a79075d7308f950da7ff1
SAITO Kazuya <saito@smartalgorithm.co.jp>
2022-11-23 14:12:04 +0900

    Create phoenix 1.7.0-rc.0 project

---
+++ README.md
@@ -0,0 +1,19 @@
+# Phoenix 1.7.0-rc.0 phx.gen.auth Sample
+
+To start your Phoenix server:
+
+  * Install Docker
+  * Build and setup with `docker compose run --rm web mix setup`
+  * Start Phoenix endpoint with `docker compose up`
+
+Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
+
+Ready to run in production? Please [check our deployment guides](https://hexdocs.pm/phoenix/deployment.html).
+
+## Learn more
+
+  * Official website: https://www.phoenixframework.org/
+  * Guides: https://hexdocs.pm/phoenix/overview.html
+  * Docs: https://hexdocs.pm/phoenix
+  * Forum: https://elixirforum.com/c/phoenix-forum
+  * Source: https://github.com/phoenixframework/phoenix
---
+++ config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :sample, Sample.Repo,
+  database: Path.expand("../sample_test.db", Path.dirname(__ENV__.file)),
+  pool_size: 5,
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :sample, SampleWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "64M4SG9+0YSCx+28ObybPvmeYtP2IMhCF+K4y8JgVdcLZPjWRZDV9AjTXOin7IPj",
+  server: false
+
+# In test we don't send emails.
+config :sample, Sample.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
